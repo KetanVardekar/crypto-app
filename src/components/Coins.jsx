95f83d80d1449b5a21b2fb840e046b58490e506d
KetanVardekar <vketan2188@gmail.com>
2024-01-24 17:12:06 +0530

    made-pages

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { server } from "..";
+import {
+  Container,
+  HStack,
+  Heading,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import Loader from "./Loader";
+import ErrorComponent from "./ErrorComponent";
+const Coins = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("inr");
 
-const Coin = () => {
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const { data } = await axios.get(
+          `${server}/coins/markets?vs_currency=${currency}&page=${page}`
+        );
+        setCoins(data);
+        console.log(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchCoins();
+  }, [currency,page]);
+  if (error)
+    return <ErrorComponent message={"Error while fetching Exchanges"} />;
+
   return (
-    <div>Coin</div>
-  )
-}
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <HStack wrap={"wrap"}>
+            {coins.map((i) => (
+              <Coin
+                key={i.id}
+                name={i.name}
+                img={i.image}
+                rank={i.market_cap_rank
+                }
+                url={i.url}
+              />
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
+};
 
-export default Coin
+const Coin = ({ name, img, rank, url }) => {
+  return (
+    <>
+      <a href={url} target={"blank"}>
+        <VStack
+          w={"52"}
+          shadow={"lg"}
+          p={"8"}
+          borderRadius={"lg"}
+          transition={"all 0.3s"}
+          m={"4"}
+          css={{
+            "&:hover": {
+              transform: "scale(1.1)",
+            },
+          }}
+        >
+          <Image
+            src={img}
+            w={"10"}
+            h={"10"}
+            objectfit={"contain"}
+            alt={"Exchanges"}
+          />
+
+          <Heading size={"md"} noOfLines={1}>
+            {rank}
+          </Heading>
+          <Text noOfLines={1}>{name}</Text>
+        </VStack>
+      </a>
+    </>
+  );
+};
+
+export default Coins;
